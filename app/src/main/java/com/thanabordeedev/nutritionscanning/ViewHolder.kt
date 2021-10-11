ba9614983a15b56c3f3959da1231597b422f8936
thanabordeedev <nook.rabbit@gmail.com>
2021-10-12 02:20:26 +0700

    Add Picasso into project

--- app/src/main/java/com/thanabordeedev/nutritionscanning/ViewHolder.kt
+++ app/src/main/java/com/thanabordeedev/nutritionscanning/ViewHolder.kt
@@ -1,4 +1,29 @@
 package com.thanabordeedev.nutritionscanning
 
-class ViewHolder {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.core.Context
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private lateinit var rImageViewIV: ImageView
+    private lateinit var scanDataTV: TextView
+    private lateinit var dateDataTV: TextView
+    private lateinit var timeDataTV: TextView
+
+    fun setDetail(content: Context,imagePath: String,scanData: String,scanDate: String, scanTime: String){
+
+        rImageViewIV = itemView.findViewById(R.id.rImageView)
+        scanDataTV = itemView.findViewById(R.id.scanData)
+        dateDataTV = itemView.findViewById(R.id.textDate)
+        timeDataTV = itemView.findViewById(R.id.textTime)
+
+
+        scanDataTV.setText(scanData)
+        dateDataTV.text = "${R.string.date2} $scanDataTV"
+        timeDataTV.text = "${R.string.time2} $timeDataTV"
+
+    }
 }
