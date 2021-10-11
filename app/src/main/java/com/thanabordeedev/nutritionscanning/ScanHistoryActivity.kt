d7fa00d98dff5cb0ed8775edd6650cb2d0931505
thanabordeedev <nook.rabbit@gmail.com>
2021-10-11 14:55:50 +0700

    Add Scan history Activity

---
+++ app/src/main/java/com/thanabordeedev/nutritionscanning/ScanHistoryActivity.kt
@@ -0,0 +1,11 @@
+package com.thanabordeedev.nutritionscanning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScanHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_history)
+    }
+}
