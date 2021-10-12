2c466c9ae10232f4ee43187182b36ac6375655b7
thanabordeedev <nook.rabbit@gmail.com>
2021-10-12 11:06:52 +0700

    ีืgit commit

--- app/src/main/java/com/thanabordeedev/nutritionscanning/ScanHistoryActivity.kt
+++ app/src/main/java/com/thanabordeedev/nutritionscanning/ScanHistoryActivity.kt
@@ -2,10 +2,42 @@ package com.thanabordeedev.nutritionscanning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.thanabordeedev.nutritionscanning.databinding.ActivityScanHistoryBinding
+import java.lang.reflect.Member
 
 class ScanHistoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScanHistoryBinding
+    private lateinit var mRecylerView: RecyclerView
+    private lateinit var mauth: FirebaseAuth
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var firebaseRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan_history)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding = ActivityScanHistoryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mauth = FirebaseAuth.getInstance()
+
+        mRecylerView = binding.recyclerview
+        mRecylerView.setHasFixedSize(true)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        firebaseRef = firebaseDatabase.getReference("ScanResult_Data").child(mauth.currentUser!!.uid)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        var firebaseRecycleAdapter: FirebaseRecyclerAdapter<Member,ViewHolder>
     }
 }
--- app/src/main/java/com/thanabordeedev/nutritionscanning/ViewHolder.kt
+++ app/src/main/java/com/thanabordeedev/nutritionscanning/ViewHolder.kt
@@ -1,10 +1,13 @@
 package com.thanabordeedev.nutritionscanning
 
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.core.Context
+import com.squareup.picasso.Picasso
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -12,18 +15,20 @@ class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     private lateinit var scanDataTV: TextView
     private lateinit var dateDataTV: TextView
     private lateinit var timeDataTV: TextView
+    private lateinit var animate: Animation
 
-    fun setDetail(content: Context,imagePath: String,scanData: String,scanDate: String, scanTime: String){
+    fun setDetail(context: Context ,imagePath: String,scanData: String,scanDate: String, scanTime: String){
 
         rImageViewIV = itemView.findViewById(R.id.rImageView)
         scanDataTV = itemView.findViewById(R.id.scanData)
         dateDataTV = itemView.findViewById(R.id.textDate)
         timeDataTV = itemView.findViewById(R.id.textTime)
 
-
+        Picasso.get().load(imagePath).into(rImageViewIV)
         scanDataTV.setText(scanData)
         dateDataTV.text = "${R.string.date2} $scanDataTV"
         timeDataTV.text = "${R.string.time2} $timeDataTV"
+
 
     }
 }
