fc379445e8de3e0995d0ebc009c0aef3efd237d9
thanabordeedev <nook.rabbit@gmail.com>
2021-10-12 16:10:29 +0700

    scan list adapter

--- app/src/main/java/com/thanabordeedev/nutritionscanning/ViewHolder.kt
+++ app/src/main/java/com/thanabordeedev/nutritionscanning/ViewHolder.kt
@@ -1,20 +1,16 @@
 package com.thanabordeedev.nutritionscanning
 
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.core.Context
-import com.squareup.picasso.Picasso
 
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     var rImageViewIV: ImageView = itemView.findViewById(R.id.rImageView)
     var scanDataTV: TextView = itemView.findViewById(R.id.scanData)
-    var dateDataTV: TextView = itemView.findViewById(R.id.textDate)
-    var timeDataTV: TextView = itemView.findViewById(R.id.textTime)
+    var dateDataTV: TextView = itemView.findViewById(R.id.textDate2)
+    var timeDataTV: TextView = itemView.findViewById(R.id.textTime2)
 
 
 }
