21bd8c2bc98432c3cac950ad527a4f432ef88b7a
thanabordeedev <nook.rabbit@gmail.com>
2021-11-05 23:42:48 +0700

    -

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -13,13 +13,13 @@ def main(data,discreaseIndex):
     for din in discreaseList:
         for index in range(len(text_cut)):
             if din == 1:
-                if text_cut[index] == "น้ำตาล":
+                if text_cut[index] == "ตาล" or text_cut[index] == "น้ำตาล":
                     discreaseResult +="1"
             elif din == 2:
                 if text_cut[index] == "โซเดียม":
                     discreaseResult +="2"
             elif din == 3:
-                if text_cut[index] == "ถั่วเหลือง":
+                if text_cut[index] == "ถั่วเหลือง" or text_cut[index] == "ถัวเหลือง":
                     discreaseResult +="3"
             elif din == 4:
                 if text_cut[index] == "ทะเล":
